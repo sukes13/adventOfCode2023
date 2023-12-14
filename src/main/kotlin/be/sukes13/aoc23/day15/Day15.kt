d97b65594b79534ffdd1d73291015f23e72dc938
sukes13 <sir_jelle_wouters@hotmail.com>
2023-12-14 18:55:43 +0100

    empty day 15

---
+++ src/main/kotlin/be/sukes13/aoc23/day15/Day15.kt
@@ -0,0 +1,5 @@
+package be.sukes13.aoc23.day15
+
+fun part1(input: String) = 0
+
+fun part2(input: String) = 0
---
+++ src/test/kotlin/be/sukes13/aoc23/day15/Day15Test.kt
@@ -0,0 +1,13 @@
+package be.sukes13.aoc23.day15
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+
+class Day15Test {
+    @Test
+    fun `parse first line of input, correct hand and bid returned`() {
+        assertThat("3").isEqualTo("3")
+    }
+
+}
