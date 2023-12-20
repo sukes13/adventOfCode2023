5999c9652a6130e9ca72769295aa7a87875b29f7
sukes13 <sir_jelle_wouters@hotmail.com>
2023-12-20 22:54:24 +0100

    solution day 5 part 1

--- src/main/kotlin/be/sukes13/aoc23/day5/SeedLocations.kt
+++ src/main/kotlin/be/sukes13/aoc23/day5/SeedLocations.kt
@@ -1,43 +1,35 @@
 package be.sukes13.aoc23.day5
 
 import be.sukes13.splitOnEmptyLine
-import javax.print.attribute.standard.Destination
-import kotlin.math.absoluteValue
 
-fun part1(input: String) = input.toDestinationMappers().toLocationNumbers(input.toSeeds()).min()
+fun part1(input: String) = input.toDestinationMappers().let { destinationMappers ->
+    input.toSeeds().minOfOrNull { destinationMappers.toLocationNumber(it) }
+}
 
 fun part2(input: String) = 0
 
-private fun List<List<DestinationMapper>>.toLocationNumbers(seeds: List<Long>): List<Long> {
-    return map { roundOfMappers ->
-        roundOfMappers.mapSeeds(seeds).also { println(it) }
-    }.last()
-
-}
-
-private fun List<DestinationMapper>.mapSeeds(seeds: List<Long>) =
-    seeds.map {
-        findMapper(it)?.mapToDestination(it) ?: it
+private fun List<List<DestinationMapper>>.toLocationNumber(seed: Long) =
+    fold(seed) { acc, roundOfMappers ->
+        roundOfMappers.findMapper(acc)?.mapToDestination(acc) ?: acc
     }
 
 private fun List<DestinationMapper>.findMapper(seed: Long) =
-    singleOrNull { seed in it.seedStart..it.seedStart + it.length }
+    singleOrNull { seed in it.seedStart until (it.seedStart + it.length) }
+
+data class DestinationMapper(val seedStart: Long, val destinationStart: Long, val length: Long) {
+    fun mapToDestination(input: Long) = destinationStart + (input - seedStart)
+}
 
 private fun String.toDestinationMappers() = splitOnEmptyLine().drop(1).map { mapperBlock ->
     mapperBlock.lines().drop(1).map { line ->
         line.split(" ").let { splitLine ->
             DestinationMapper(
-                seedStart = splitLine.first().toLong(),
-                destinationStart = splitLine[1].toLong(),
+                seedStart = splitLine[1].toLong(),
+                destinationStart = splitLine.first().toLong(),
                 length = splitLine.last().toLong()
             )
         }
     }
 }
 
-private fun String.toSeeds() = lines().first().replace("seeds: ", "").split(" ").map(String::toLong)
-data class DestinationMapper(val seedStart: Long, val destinationStart: Long, val length: Long) {
-    fun mapToDestination(input: Long): Long {
-        return destinationStart + (destinationStart - seedStart).absoluteValue + input
-    }
-}
+private fun String.toSeeds() = lines().first().split(" ").drop(1).map(String::toLong)
